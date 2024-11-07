ce25975b08eaf05c8c6ef853aee23b3dfd5e4c82
unknown <ar.eec@mail.ru>
2024-11-07 19:08:28 +0500

    черный ящик и немного курсача

---
+++ Курсовая работа РПМ/ProjectOrg/DealPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectOrg
+{
+    /// <summary>
+    /// Логика взаимодействия для DealPage.xaml
+    /// </summary>
+    public partial class DealPage : Page
+    {
+        public DealPage()
+        {
+            InitializeComponent();
+            var currentDesingWorks = ProjectOranizationDBEntities2.GetContext().DesingWork.ToList();
+            DesingWorkListView.ItemsSource = currentDesingWorks;
+            SortBox.SelectedIndex = 0;
+            FiltBox.SelectedIndex = 0;
+            Update();
+        }
+
+       void Update()
+        {
+            var currentDesingWorks = ProjectOranizationDBEntities2.GetContext().DesingWork.ToList();
+
+            if(SortBox.SelectedIndex == 1) currentDesingWorks = currentDesingWorks.OrderBy(p => p.EndDate).ToList();
+            if (SortBox.SelectedIndex == 2) currentDesingWorks = currentDesingWorks.OrderByDescending(p => p.EndDate).ToList();
+
+            if(FiltBox.SelectedIndex == 1) currentDesingWorks = currentDesingWorks.Where(p => (p.DepartmentID == 1)).ToList();
+            if(FiltBox.SelectedIndex == 2) currentDesingWorks = currentDesingWorks.Where(p =>  p.DepartmentID == 2).ToList();
+            if(FiltBox.SelectedIndex == 3) currentDesingWorks = currentDesingWorks.Where(p => (p.DepartmentID == 3)).ToList();
+            if(FiltBox.SelectedIndex == 4) currentDesingWorks = currentDesingWorks.Where(p => (p.DepartmentID == 4)).ToList();
+            if(FiltBox.SelectedIndex == 5) currentDesingWorks = currentDesingWorks.Where(p => (p.DepartmentID == 5)).ToList();
+
+            currentDesingWorks = currentDesingWorks.Where(p => (p.dealID.ToString().Contains(SearchTxt.Text))).ToList();
+
+            DesingWorkListView.ItemsSource = currentDesingWorks;
+        }
+
+        private void SortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void SearchTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void FiltBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+        }
+    }
+}
